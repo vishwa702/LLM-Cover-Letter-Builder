623fce3ee344ad5e94e83addf64dd2bcddf94c3e
Vishwa <vtohal@gmail.com>
2025-03-12 18:24:04 -0600

    Add icons to sidebar

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ import streamlit as st
 
 pages = {
     "Navigation": [
-        st.Page("pages/Resume_Builder.py", title="Resume Builder"),
-        st.Page("pages/File_Management.py", title="File Management"),
+        st.Page("pages/Resume_Builder.py", title="Resume Builder", icon=':material/rocket_launch:'),
+        st.Page("pages/File_Management.py", title="File Management", icon=':material/folder:'),
     ],
 
 }
